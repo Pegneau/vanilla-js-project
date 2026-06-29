47ce24dfd663e7bab44491536d233f5e41df51a1
Rodrigo Lourenzutti Pegneau <rodrigopegneau@gmail.com>
2026-06-29 16:55:35 +0200

    feat: added ProductItem Component, and fixed directories

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -8,9 +8,9 @@ export class MenuPage extends HTMLElement {
     this.root.appendChild(styles);
 
     async function loadCSS() {
-      const request = await fetch("/components/MenuPage.css");
+      const request = await fetch(new URL("./MenuPage.css", import.meta.url));
       const css = await request.text();
-      styles.testContent = css;
+      styles.textContent = css;
     }
     loadCSS();
   }
@@ -18,6 +18,33 @@ export class MenuPage extends HTMLElement {
     const template = document.getElementById("menu-page-template");
     const content = template.content.cloneNode(true);
     this.root.appendChild(content);
+
+    window.addEventListener("appmenuchange", () => {
+      this.render();
+    });
+    this.render();
+  }
+  render() {
+    if (app.store.menu) {
+      this.root.querySelector("#menu").innerHTML = "";
+      for (let category of app.store.menu) {
+        const liCategory = document.createElement("li");
+        liCategory.innerHTML = `
+        <h3>${category.name}</h3>
+            <ul class='category'>
+            </ul>
+            `;
+        this.root.querySelector("#menu").appendChild(liCategory);
+
+        category.products.forEach((product) => {
+          const item = document.createElement("product-item");
+          item.dataset.product = JSON.stringify(product);
+          liCategory.querySelector("ul").appendChild(item);
+        });
+      }
+    } else {
+      this.root.querySelector("#menu").innerHTML = "Loading...";
+    }
   }
 }
 customElements.define("menu-page", MenuPage);
--- services/API.js
+++ services/API.js
@@ -1,7 +1,6 @@
 const API = {
-  url: "/data/menu.json",
   fetchMenu: async () => {
-    const result = await fetch(API.url);
+    const result = await fetch(new URL("../data/menu.json", import.meta.url));
     return await result.json();
   },
 };
